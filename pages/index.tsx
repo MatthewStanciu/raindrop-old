3007138b3f48b073242c810e8ac1a005e5795c82
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-20 23:24:48 +0300

    Dark mode by default

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,14 @@ const App = () => {
   const { user, isLoading } = useUser()
 
   return user ? (
-    <div className="max-w-3/4 flex min-h-screen flex-col py-2">
+    <div className="max-w-3/4 flex min-h-screen flex-col bg-black py-2 text-white">
       {isLoading ? null : (
         <div>
           <Head>
             <title>💧 Raindrop</title>
           </Head>
           <button
-            className="max-w-fit rounded-lg border-2 border-black bg-black px-2 text-white"
+            className="max-w-fit rounded-lg border-2 border-white bg-white px-2 text-black"
             onClick={() => supabaseClient.auth.signOut()}
           >
             Sign out
@@ -25,12 +25,12 @@ const App = () => {
       )}
     </div>
   ) : (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black py-2 text-white">
       <Head>
         <title>Sign In</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col gap-y-4 rounded-lg bg-gray-200 p-4 text-left">
+      <div className="flex flex-col gap-y-4 rounded-lg bg-gray-700 p-4 text-left">
         <h1 className="text-xl font-bold">Sign In</h1>
         <button
           className="flex transform flex-row gap-2 rounded-lg bg-black p-2 text-white transition hover:scale-105"
