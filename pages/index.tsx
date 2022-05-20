7c80cb0e2571be0f37652d30f85125ee3df68ab4
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-20 19:56:17 +0300

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,59 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { Github } from '@icons-pack/react-simple-icons'
+import { useUser } from '@supabase/supabase-auth-helpers/react'
+import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const App = () => {
+  const { user, isLoading } = useUser()
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+  if (!user) {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <Head>
+          <title>Sign In</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="flex flex-col gap-y-4 rounded-lg bg-gray-200 p-4 text-left">
+          <h1 className="text-xl font-bold">Sign In</h1>
+          <button
+            className="flex transform flex-row gap-2 rounded-lg bg-black p-2 text-white transition hover:scale-105"
+            onClick={async (e) => {
+              e.preventDefault()
+              await supabaseClient.auth.signIn(
+                {
+                  provider: 'github',
+                },
+                { redirectTo: window.location.href }
+              )
+            }}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <Github />
+            <p>Sign in with GitHub</p>
+          </button>
         </div>
-      </main>
+      </div>
+    )
+  }
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+  return (
+    <div className="max-w-3/4 flex min-h-screen flex-col py-2">
+      {isLoading ? null : (
+        <div>
+          <Head>
+            <title>💧 Raindrop</title>
+          </Head>
+          <button
+            className="max-w-fit rounded-lg border-2 border-black bg-black px-2 text-white"
+            onClick={() => supabaseClient.auth.signOut()}
+          >
+            Sign out
+          </button>
+          <p>user:</p>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </div>
+      )}
     </div>
   )
 }
 
-export default Home
+export default App
