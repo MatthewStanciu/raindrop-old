cbe1a789868b742e9acc9b727c2806f1a82353db
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-21 13:46:48 +0300

    Restrict to Purdue Hackers GitHub org

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,42 @@
 import Head from 'next/head'
-import { Github } from '@icons-pack/react-simple-icons'
 import { useUser } from '@supabase/supabase-auth-helpers/react'
 import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
+import SignIn from '../components/SignIn'
+import { useEffect, useState } from 'react'
+import isInOrg from '../lib/is-in-org'
+import NotInOrg from '../components/NotInOrg'
 
 const App = () => {
   const { user, isLoading } = useUser()
+  const [inOrg, setInOrg] = useState(false)
+  useEffect(() => {
+    isInOrg(user?.user_metadata.user_name).then((org) => setInOrg(org))
+  })
 
   return user ? (
-    <div className="max-w-3/4 flex min-h-screen flex-col bg-black py-2 text-white">
-      {isLoading ? null : (
-        <div>
-          <Head>
-            <title>💧 Raindrop</title>
-          </Head>
-          <button
-            className="max-w-fit rounded-lg border-2 border-white bg-white px-2 text-black"
-            onClick={() => supabaseClient.auth.signOut()}
-          >
-            Sign out
-          </button>
-          <p>user:</p>
-          <pre>{JSON.stringify(user, null, 2)}</pre>
-        </div>
-      )}
-    </div>
-  ) : (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-black py-2 text-white">
-      <Head>
-        <title>Sign In</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="flex flex-col gap-y-4 rounded-lg bg-gray-700 p-4 text-left">
-        <h1 className="text-xl font-bold">Sign In</h1>
-        <button
-          className="flex transform flex-row gap-2 rounded-lg bg-black p-2 text-white transition hover:scale-105"
-          onClick={async (e) => {
-            e.preventDefault()
-            await supabaseClient.auth.signIn(
-              {
-                provider: 'github',
-              },
-              { redirectTo: window.location.href }
-            )
-          }}
-        >
-          <Github />
-          <p>Sign in with GitHub</p>
-        </button>
+    inOrg ? (
+      <div className="max-w-3/4 flex min-h-screen flex-col bg-black py-2 text-white">
+        {isLoading ? null : (
+          <div>
+            <Head>
+              <title>💧 Raindrop</title>
+            </Head>
+            <button
+              className="max-w-fit rounded-lg border-2 border-white bg-white px-2 text-black"
+              onClick={() => supabaseClient.auth.signOut()}
+            >
+              Sign out
+            </button>
+            <p>user:</p>
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+          </div>
+        )}
       </div>
-    </div>
+    ) : (
+      <NotInOrg />
+    )
+  ) : (
+    <SignIn />
   )
 
   // if (!user) {
