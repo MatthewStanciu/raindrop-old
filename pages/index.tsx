bfb86b262b4c73675fcfb700b0c40edb8be7114f
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-21 13:53:17 +0300

    Remove large comment block

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,55 +38,6 @@ const App = () => {
   ) : (
     <SignIn />
   )
-
-  // if (!user) {
-  //   return (
-  //     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-  //       <Head>
-  //         <title>Sign In</title>
-  //         <link rel="icon" href="/favicon.ico" />
-  //       </Head>
-  //       <div className="flex flex-col gap-y-4 rounded-lg bg-gray-200 p-4 text-left">
-  //         <h1 className="text-xl font-bold">Sign In</h1>
-  //         <button
-  //           className="flex transform flex-row gap-2 rounded-lg bg-black p-2 text-white transition hover:scale-105"
-  //           onClick={async (e) => {
-  //             e.preventDefault()
-  //             await supabaseClient.auth.signIn(
-  //               {
-  //                 provider: 'github',
-  //               },
-  //               { redirectTo: window.location.href }
-  //             )
-  //           }}
-  //         >
-  //           <Github />
-  //           <p>Sign in with GitHub</p>
-  //         </button>
-  //       </div>
-  //     </div>
-  //   )
-  // }
-
-  // return (
-  //   <div className="max-w-3/4 flex min-h-screen flex-col py-2">
-  //     {isLoading ? null : (
-  //       <div>
-  //         <Head>
-  //           <title>💧 Raindrop</title>
-  //         </Head>
-  //         <button
-  //           className="max-w-fit rounded-lg border-2 border-black bg-black px-2 text-white"
-  //           onClick={() => supabaseClient.auth.signOut()}
-  //         >
-  //           Sign out
-  //         </button>
-  //         <p>user:</p>
-  //         <pre>{JSON.stringify(user, null, 2)}</pre>
-  //       </div>
-  //     )}
-  //   </div>
-  // )
 }
 
 export default App
