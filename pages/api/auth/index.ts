24a351506e85f94d3eab9ec64a704b829b219add
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-27 10:18:28 -0700

    Start over

--- pages/api/auth/index.ts
+++
@@ -1,6 +0,0 @@
-import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  supabaseClient.auth.api.setAuthCookie(req, res)
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,3 @@
-import Head from 'next/head'
-import { useUser } from '@supabase/supabase-auth-helpers/react'
-import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
-import SignIn from '../components/sign-in'
-import { useEffect, useState } from 'react'
-import isInOrg from '../lib/is-in-org'
-import NotInOrg from '../components/not-in-org'
-import { createUser, userIsInDb } from '../lib/manage-user'
-import User from '../components/user'
+const Index = () => <h1>hi</h1>
 
-const App = () => {
-  const { user, isLoading } = useUser()
-  const username = user?.user_metadata.user_name
-  const [inOrg, setInOrg] = useState(false)
-
-  useEffect(() => {
-    isInOrg(username).then((org) => setInOrg(org))
-    const { data } = supabaseClient.auth.onAuthStateChange((event, session) => {
-      fetch('/api/auth', {
-        method: 'POST',
-        headers: new Headers({ 'Content-Type': 'application/json' }),
-        credentials: 'same-origin',
-        body: JSON.stringify({ event, session })
-      }).then((res) => res.json())
-    })
-  }, [user])
-
-  return user ? inOrg ? isLoading ? null : <User /> : <NotInOrg /> : <SignIn />
-}
-
-export default App
+export default Index
