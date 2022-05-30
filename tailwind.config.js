4ef345883e21d061e21b1fb9960ef29e090dcb63
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-30 00:08:45 -0700

    IBM Plex Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: '"IBM Plex Sans", system-ui, Roboto, sans-serif',
+      mono: '"IBM Plex Mono"'
+    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
